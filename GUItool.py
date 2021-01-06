c06836d54bb791acebfe420c0e678d45715e0b9b
Jelmer <66646667+Jelmer1887@users.noreply.github.com>
2021-01-06 13:17:19 +0100

    added python postfix to GUItool file, cleaned up a LOT of code
    look at the !LOOK! markings in the file please... There's a lot that we can clean up

--- GUItool.py
+++ GUItool.py
@@ -1,3 +1,4 @@
+# imports
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, output_file, show
 from bokeh.models.widgets import Panel, Tabs
@@ -13,27 +14,80 @@ from numpy import arange
 from itertools import chain
 from collections import OrderedDict
 
-plot = figure(tools="pan,wheel_zoom,box_zoom,reset")
-plot.add_tools(BoxSelectTool(dimensions="width"))
-output_file("test.html")
-df = pd.read_excel(
-    r'C:\dataset.xlsx')
+# why is 'plot' created??? I can't find any place that add any data to it... (see p1)
+plot = figure(tools="pan,wheel_zoom,box_zoom,reset")    # create figure object
+plot.add_tools(BoxSelectTool(dimensions="width"))       # add interactive tools to plot
+output_file("test.html")                                # set output path
+df = pd.read_excel(                                     # dataset path (relative to script)
+    r'archive\dataset.xlsx')
 
 #tab 1 - Assignment 4 visualisation
-regularWardPos = [0, 0, 0, 0, 0]
-semiIntensivePos = [0, 0, 0, 0, 0]
-intensivePos = [0, 0, 0, 0, 0]
+# !LOOK! the age_quantiles are binned per 4 entries... BUT ITS HARDCODED!!!
+# what if we want to increase or decreate the binsize? than we need to add or remove a lot of if/elif statements
+# we can put it in a loop to simplify it (see below)
+# consider using the following aproach:
+'''
+binnumber = 5                               # number of bins
+binsize = int(20/binnumber)                 # number of age_quantiles in one bin
 
-totalAgePos = [0, 0, 0, 0, 0]
+# lists to store number of people per category per bin in
+regularWardPos   = [0]*binsize              # creates a list of zeroes of length 'binsize'
+semiIntensivePos = [0]*binsize
+intensivePos     = [0]*binsize
+
+regularWardNeg   = [0]*binsize
+semiIntensiveNeg = [0]*binsize
+intensiveNeg     = [0]*binsize
+
+totalAgePos      = [0]*binsize
+totalAgeNeg      = [0]*binsize
+'''
+regularWardPos = [0]*5                      # list of zeroes for each bin and type of patient (see second visualisation for explanetion of list of zeroes)
+semiIntensivePos = [0]*5
+intensivePos = [0]*5
+
+regularWardNeg = [0]*5
+semiIntensiveNeg = [0]*5
+intensiveNeg = [0]*5
+
+totalAgePos = [0]*5
+totalAgeNeg = [0]*5
 
 
-regularWardNeg = [0, 0, 0, 0, 0]
-semiIntensiveNeg = [0, 0, 0, 0, 0]
-intensiveNeg = [0, 0, 0, 0, 0]
+for index, row in df.iterrows():                        # iterate over dataframe rows
 
-totalAgeNeg = [0, 0, 0, 0, 0]
+    # extracting number of patients per age group per ward per test result
+    # The bins follow a pattern, which can thus be put into a loop to prevent repetition, replace 93 lines of code with 26:
+    '''
+    for bin in binnumber:                       # iterate over the bins
+        binStartAge = bin * binsize             # computes at which age quantile a bin starts
 
-for index, row in df.iterrows():
+        # checks if age_quantile at 'index' is in the current bin:
+        if binStartAge <= df.iloc[index, 1] and df.iloc[index, 1] < (binStartAge + binnumber):
+
+            if df.iloc[index, 2] == "positive":     # if patient is positive...
+                totalAgePos[bin] += 1               # ... increment counter for number of people in current bin that are positive
+
+                # identify at which ward the *positive* patient is in and increment corresponding counter
+                if df.iloc[index, 3] == 1:
+                    regularWardPos[bin] += 1
+                elif df.iloc[index, 4] == 1:
+                    semiIntensivePos[bin] += 1
+                elif df.iloc[index, 5] == 1:
+                    intensivePos[bin] += 1
+
+            if df.iloc[index, 2] == "negative":     # if patient is negative...
+                totalAgeNeg[bin] += 1               # ... increment counter for number of people in current bin that are negative
+
+                # identify at which ward the *negative* patient is in and increment corresponding counter
+                if df.iloc[index, 3] == 1:
+                    regularWardNeg[bin] += 1
+                elif df.iloc[index, 4] == 1:
+                    semiIntensiveNeg[bin] += 1
+                elif df.iloc[index, 5] == 1:
+                    intensiveNeg[bin] += 1  
+    '''
+
     if df.iloc[index, 1] == 0 or df.iloc[index, 1] == 1 or df.iloc[index, 1] == 2 or df.iloc[index, 1] == 3:
         if df.iloc[index, 2] == "positive":
             totalAgePos[0] += 1
@@ -129,15 +183,17 @@ for index, row in df.iterrows():
             elif df.iloc[index, 5] == 1:
                 intensiveNeg[4] += 1
 
-percentageRegularWardPos = [0, 0, 0, 0, 0]
-percentageSemiIntensivePos = [0, 0, 0, 0, 0]
-percentageIntensivePos = [0, 0, 0, 0, 0]
+# lists of zeroes to store percentages per bin per type
+percentageRegularWardPos   = [0]*5      # !LOOK! replaced [0,0,0,0,0] with [0]*5... the 5 shouldn't be hardcoded (change to binnumber??? (see above))
+percentageSemiIntensivePos = [0]*5
+percentageIntensivePos     = [0]*5
 
-percentageRegularWardNeg = [0, 0, 0, 0, 0]
-percentageSemiIntensiveNeg = [0, 0, 0, 0, 0]
-percentageIntensiveNeg = [0, 0, 0, 0, 0]
+percentageRegularWardNeg   = [0]*5
+percentageSemiIntensiveNeg = [0]*5
+percentageIntensiveNeg     = [0]*5
 
 for i in range(len(regularWardPos)):
+    # compute percentages of positive and negative patients per age group per ward
     percentageRegularWardPos[i] = regularWardPos[i] / totalAgePos[i] * 100
     percentageSemiIntensivePos[i] = semiIntensivePos[i] / totalAgePos[i] * 100
     percentageIntensivePos[i] = intensivePos[i] / totalAgePos[i] * 100
@@ -147,64 +203,113 @@ for i in range(len(regularWardPos)):
     percentageIntensiveNeg[i] = intensiveNeg[i] / totalAgeNeg[i] * 100
 
 
-
+# Legend and axis labels
 wardDevision = ["regular ward", "semi-intensive unit", "intensive care"]
-
 ageDevision = ["child and teen", "young adult and adult", "middle aged", "senior", "elderly"]
-
 positiveReg = ["regular ward positive", "regular ward negative"]
 positiveSemi = ["semi-intensive unit positive", "semi-intensive unit negative"]
 positiveIntens = ["intensive care positive", "intensive care negative"]
 
-dictDataReg = {'age group': ageDevision,
-            "regular ward positive" : percentageRegularWardPos,
-            "regular ward negative" : percentageRegularWardNeg}
+# packaging data into dictionary format
+dictDataReg = {
+    'age group': ageDevision,
+    "regular ward positive" : percentageRegularWardPos,
+    "regular ward negative" : percentageRegularWardNeg
+}
+dictDataSemi = {
+    'age group': ageDevision,
+    "semi-intensive unit positive" : percentageSemiIntensivePos,
+    "semi-intensive unit negative" : percentageSemiIntensiveNeg
+}
+dictDataIntens = {
+    'age group': ageDevision,
+    "intensive care positive": percentageIntensivePos,
+    "intensive care negative": percentageIntensiveNeg
+}
 
-dictDataSemi = {'age group': ageDevision,
-            "semi-intensive unit positive" : percentageSemiIntensivePos,
-            "semi-intensive unit negative" : percentageSemiIntensiveNeg}
-
-
-dictDataIntens = {'age group': ageDevision,
-            "intensive care positive": percentageIntensivePos,
-            "intensive care negative": percentageIntensiveNeg}
-
+# converting dictionaries into datasources
 sourceReg = ColumnDataSource(data=dictDataReg)
 sourceSemi = ColumnDataSource(data=dictDataSemi)
 sourceIntens = ColumnDataSource(data=dictDataIntens)
 
-colorsReg = ["#2874A6", "#85C1E9"]
-colorsSemi = ["#B03A2E", "#F5B7B1"]
-colorsIntens = ["#B7950B", "#F9E79F"]
+# colors for the bars
+colorsReg = ["#2874A6", "#85C1E9"]      # regular ward color  [positive test, negative test]
+colorsSemi = ["#B03A2E", "#F5B7B1"]     # semi-intense ward color [positive test, negative test]
+colorsIntens = ["#B7950B", "#F9E79F"]   # intensive ward color  [positive test, negative test]
 
-p1 = figure(x_range=ageDevision,
-               title="Percentage of age group with positive Covid-19 test in hospital ward", toolbar_location=None,
-               tools="")
+p1 = figure(
+    x_range=ageDevision,                # sets x-axis
+    title="Percentage of age group with positive Covid-19 test in hospital ward", # title of the plot
+    toolbar_location=None,              # sets location of the toolbar (nowhere)
+    tools=""                            # disables tools
+)
 
 #dodge('age group', -0.25, range=chart.x_range)
 
-p1.vbar_stack(positiveReg, x=dodge('age group', -0.25, range=p1.x_range), width=0.2, source=dictDataReg,
-           color= colorsReg, legend_label=positiveReg)
+# adds stacked bars to the plot
+p1.vbar_stack(
+    positiveReg,                        # sets y-axis for bars (using keys of source)
+    x=dodge('age group', -0.25, range=p1.x_range), # sets x-axis
+    width=0.2,                          # width of bars
+    source=dictDataReg,                 # datasource for the bars
+    color= colorsReg,                   # colors of the bars
+    legend_label=positiveReg            # set legend label
+)
 
-p1.vbar_stack(positiveSemi, x=dodge('age group', 0.0, range=p1.x_range), width=0.2, source=dictDataSemi,
-           color= colorsSemi, legend_label=positiveSemi)
+p1.vbar_stack(
+    positiveSemi,                       # sets y-axis for bars (using keys of source)
+    x=dodge('age group', 0.0, range=p1.x_range), # sets x-axis
+    width=0.2,                          # width of bars
+    source=dictDataSemi,                # datasource for the bars
+    color= colorsSemi,                  # colors of the bars
+    legend_label=positiveSemi           # set legend label
+)
 
-p1.vbar_stack(positiveIntens, x=dodge('age group', 0.25, range=p1.x_range), width=0.2, source=dictDataIntens,
-           color= colorsIntens, legend_label=positiveIntens)
+p1.vbar_stack(
+    positiveIntens,                     # sets y-axis for bars (using keys of source)
+    x=dodge('age group', 0.25, range=p1.x_range), # sets x-axis
+    width=0.2,                          # width of bars
+    source=dictDataIntens,              # datasource for the bars
+    color= colorsIntens,                # colors of the bars
+    legend_label=positiveIntens         # set legend label
+)
 
-p1.y_range.start = 0
-p1.x_range.range_padding = 0.1
-p1.xgrid.grid_line_color = None
-p1.legend.location = "top_left"
-p1.legend.orientation = "vertical"
+# plot properties
+p1.y_range.start = 0                    # y-range starting point
+p1.x_range.range_padding = 0.1          # additional space between x-axis values
+p1.xgrid.grid_line_color = None         # removes gridlines
+p1.legend.location = "top_left"         # sets location of Legend
+p1.legend.orientation = "vertical"      # sets orientation of the Legend (vertical -> entries below each other)
 
 
 #Visualisation 2 - Bar chart - Assignment 3
-positiveAge = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-totalAge = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# !LOOK! instead of putting 20 zeroes in a list manually, consider using a list comprehension or a lsit operation
+'''
+positiveAge = [0 for iter in range(0,20)]   # list comprehension (puts 0 in the list as it loops 20 times)
+positiveAge = [0]*20                        # list operation (multiply [0] by 20 => [0]+[0]+...+[0] = [0, 0, ..., 0])
+
+# make lists of zeroes to store number of patients in:
+positiveAge   = [0]*20
+totalAge      = [0]*20
+percentageAge = [0]*20
+'''
+positiveAge   = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+totalAge      = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 percentageAge = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-for index, row in df.iterrows():
+for index, row in df.iterrows():        # iterate over dataset
+    # !LOOK!
+    # contains 'if/elif' statement for each age quantile... this is quite bad practise: 
+    # say we want to change "positive" to "negative", then we need to change it 20 times!
+    # rule of thumb: if you're repeating an if statement with only one number that's changing, you can make it shorter!
+    # in this case: consider using the index of the 'totalAge' as a comparison, which would replace 80 lines of code with 4:
+    '''
+    for age_quantile in range(len(totalAge)):   # loops over integers 0 to 19
+        totalAge[age_quantile] += 1             # increments counter for number of people of age 'age_quantile'
+        if df.iloc[index, 2] == "positive":     # if patient is positive...
+            positiveAge[age_quantile] += 1      # ... increment counter for number of people of age 'age_quantile' that is positive
+    '''
+
     if df.iloc[index, 1] == 0:
         totalAge[0] += 1
         if df.iloc[index, 2] == "positive":
@@ -286,22 +391,36 @@ for index, row in df.iterrows():
         if df.iloc[index, 2] == "positive":
             positiveAge[19] += 1
 
-for i in range(len(positiveAge)):
-    percentageAge[i] = positiveAge[i] / totalAge[i] *100
+for age in range(len(positiveAge)):       # compute percentage of positvely tested patients per age quantile
+    percentageAge[age] = positiveAge[age] / totalAge[age] *100
 
-print(positiveAge)
-print(totalAge)
-print(percentageAge)
+print(positiveAge)      # debugging
+print(totalAge)         # debugging
+print(percentageAge)    # debugging
 
+# !LOOK! list of labels for the x-axis of the plot
+# theres a pattern in the numbers, so it can be simplified! Consider using a list comprehension:
+# ageQuantile = [str(number) for number in range(20)]  # generates a list of numbers from 0 to 19 in string format
 ageQuantile = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"]
 
-p2 = figure(x_range=ageQuantile, plot_height=250, title="Percentage positive tests per age quartile",
-           toolbar_location=None, tools="")
+# create plot 2 object
+p2 = figure(
+    x_range=ageQuantile,                        # x-axis labels / range
+    plot_height=250,                            # height of the plot in pixels
+    title="Percentage positive tests per age quartile", # title of the plot
+    toolbar_location=None,                      # loaction of the toolbar (None -> nowhere)
+    tools=""                                    # disables toolbar/tools
+)
+# adds bars to the plot 2 object
+p2.vbar(
+    x=ageQuantile,                              # specifies at which x-axis each bar should be placed
+    top=percentageAge,                          # specifies y-axis (height of the bars)
+    width=0.5                                   # specifies width of the bars
+)
 
-p2.vbar(x=ageQuantile, top=percentageAge, width=0.5)
-
-p2.xgrid.grid_line_color = None
-p2.y_range.start = 0
+# plot 2 properties
+p2.xgrid.grid_line_color = None                 # disables gridlines
+p2.y_range.start = 0                            # sets starting point for y-axis
 
 #Visualisation 3 - Heat map - Assignment 3
 dfVirus = df.iloc[:,  21:38]
